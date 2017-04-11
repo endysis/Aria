5ce06847ab286eff6d03ad53617bfd6568bf306d
Stefano Rinaldi <14480397@students.lincoln.ac.uk>
2017-04-11 19:16:12 +0100

    Ported over to phone and scrolling

--- Aria/js/j.js
+++ Aria/js/j.js
@@ -1,10 +1,3 @@
-$(document).ready(function($) {
-
-    $('.card__share > a').on('click', function(e){ 
-        e.preventDefault() // prevent default action - hash doesn't appear in url
-        $(this).parent().find( 'div' ).toggleClass( 'card__social--active' );
-        $(this).toggleClass('share-expanded');
-    });
-  
-});
- 
+$(document).ready(function () {
+   swiper = new Swiper('.swiper-container'); 
+});	  
