3eedf3566b488c0337bbfc35a3a83af110bd6f66
endysis <shea_o@me.com>
2017-04-15 02:24:16 +0100

    Try to scroll

--- Aria/js/j.js
+++ Aria/js/j.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
    swiper = new Swiper('.swiper-container'); 
+       var swiper = new Swiper('.swiper-containerSave', {
+        scrollbar: '.swiper-scrollbar',
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true
+    });
 });	  
