b2c9417a9124410ef7dd96838ae0c6c7a7d28aed
Andrew Cardwell <andrewcardwell@btinternet.com>
2017-04-15 19:02:56 +0100

    Added info page

--- Aria/js/j.js
+++ Aria/js/j.js
@@ -1,10 +1,3 @@
 $(document).ready(function () {
    swiper = new Swiper('.swiper-container'); 
-       var swiper = new Swiper('.swiper-containerSave', {
-        scrollbar: '.swiper-scrollbar',
-        direction: 'vertical',
-        slidesPerView: 'auto',
-        mousewheelControl: true,
-        freeMode: true
-    });
 });	  
